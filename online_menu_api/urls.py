da2c9ee7d32c486f86babd33011e7d76bea6cde8
pouria-drd <pouriadrdgmail.com>
2025-05-20 11:55:24 +0330

    added required packges and setup the settings

--- online_menu_api/urls.py
+++ online_menu_api/urls.py
@@ -1,22 +1,32 @@
-"""
-URL configuration for online_menu_api project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
+import os
+from dotenv import load_dotenv
 from django.contrib import admin
-from django.urls import path
+from django.conf import settings
+from rest_framework import routers
+from django.urls import path, include
+from django.conf.urls.static import static
+
+# Loads the variables from the .env file into the environment
+load_dotenv()
+
+router = routers.DefaultRouter()
+
+
+base_url: str = os.getenv("BASE_URL", "")
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path(base_url, include(router.urls)),
+    # main admin
+    path(base_url + os.getenv("ADMIN_URL", "admin/"), admin.site.urls),
+    # api version 1
+    path(base_url + "users/", include("users.urls")),
 ]
+
+if settings.DEBUG:
+    urlpatterns += (path("__debug__/", include("debug_toolbar.urls")),)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
+admin.site.index_title = "Online Menu"
+admin.site.site_header = "Online Menu Admin"
+admin.site.site_title = "Online Menu"
